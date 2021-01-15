694bd8a85295708349cf84690e5b3ca4343bb6d0
ErickHDdS <55605746+ErickHDdS@users.noreply.github.com>
2021-01-14 22:10:49 -0300

    Update

--- src/helpers/SynchronizePulsars.ts
+++ src/helpers/SynchronizePulsars.ts
@@ -9,7 +9,7 @@ const synchronizePulsars = (timeMachine:TimeMachine) => {
      * CALC
      */
     const spaceTime = calculateSpaceTime(timeMachine);
-    const SynchronizePulsars = Math.sqrt(Math.pow(timeMachine.currentPosition.x,2) + (Math.pow(timeMachine.currentPosition.y,2) + (Math.pow(timeMachine.currentPosition.z,2)/spaceTime)));
+    const SynchronizePulsars = Math.sqrt(Math.pow(timeMachine.currentPosition.x,2) + Math.pow(timeMachine.currentPosition.y,2) + Math.pow(timeMachine.currentPosition.z,2)/spaceTime);
     return SynchronizePulsars;
 }
 
